dcc55789bea199b8da479cf3a165012d1ab71632
martinezcarrillo <camilocrack33@gmail.com>
2023-04-30 11:02:36 -0500

    Create script.js

---
+++ Fase_4/script.js
@@ -0,0 +1,22 @@
+function procesarRespuestasFlexBox () {
+    let total = 5;
+    let puntos = 0;
+
+    let myFrom = document.forms["formulario"];
+    let respuestasCorrectas = ["c","a","a","a","a"];
+
+    for (let i = 1; i <= total; i++) {
+        if (myFrom["r" + i].value == null || 
+            myFrom["r" + i].value == "") {
+            alert("Por favor responda todas las preguntas antes de continuar ");
+            return false;
+        } else { 
+            if (myFrom["r" + i].value === respuestasCorrectas[i - 1])
+                puntos++;
+        }
+          
+    }
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML=" Obtuviste"+ puntos +"puntos de "+ total +" Posibles ";
+}
